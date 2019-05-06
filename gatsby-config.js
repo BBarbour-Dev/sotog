b59a32d9b711f27de27b3d8fe2fc070463fda606
Brian Barbour <steelvoltage@gmail.com>
2019-05-05 22:57:03 -0400

    adding more gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  siteMetadata: {
+    title: `Saga of the Old Gods`,
+    description: `Brie's story, as she learns the secrets of her past and the influence of the Old Gods on the world.`,
+    author: `KJ Sylva`,
+  },
   plugins: [
     `gatsby-plugin-netlify-cms`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -14,6 +22,13 @@ module.exports = {
       options: {
         name: "chapters",
         path: `${__dirname}/chapters`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/assets`,
       },
     },
   ],
