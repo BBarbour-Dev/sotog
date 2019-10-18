e716c8ed894e6afafcf7eda712386fe4849e48ad
Brian Barbour <brian.barbour@communitybrands.com>
2019-10-18 09:41:45 -0400

    sanity studio setup and config

---
+++ sanity-sotog/schemas/chapter.js
@@ -0,0 +1,51 @@
+export default {
+  name: 'chapter',
+  title: 'Chapter',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: { type: 'author' }
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime'
+    },
+    {
+      name: 'cover_image',
+      title: 'Cover Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text'
+    },
+    {
+      name: 'pages',
+      title: 'Pages',
+      type: 'array',
+      of: [{ type: 'image' }]
+    }
+  ]
+};
