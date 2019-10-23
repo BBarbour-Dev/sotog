37dc4859001b7043f31fcc1c5cc302da204b1f50
Brian Barbour <steelvoltage@gmail.com>
2019-10-23 16:33:36 -0400

    mobile header

---
+++ version2/components/header/mobile-header.vue
@@ -0,0 +1,65 @@
+<template>
+  <header>
+    <img class="logo-image" src="~/assets/logo.png" alt="Saga of the Old Gods" />
+    <h2>After experiencing a prophecy dream about war and destruction, Brie seeks out the truth behind them only to find out that there are far more details about her existence and who she truly is than she could have ever imagined.</h2>
+    <h3>New to the series?</h3>
+    <button class="start-here">
+      <nuxt-link to="/chapter/chapter-one">Start From Beginning</nuxt-link>
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "mobile-header"
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  min-height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)),
+    url("~assets/ravenwing.png");
+  background-attachment: fixed;
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.logo-image {
+  max-width: 100%;
+  min-width: 400px;
+  margin-top: 15vh;
+}
+
+h2 {
+  padding: 2rem;
+  text-align: justify;
+  font-size: 1.2rem;
+  font-weight: 400;
+}
+
+h3 {
+  padding: 2rem;
+  font-style: italic;
+}
+
+.start-here {
+  background: none;
+  border: none;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: bold;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+}
+
+.start-here a {
+  font-family: var(--secondary-font-face);
+  text-decoration: none;
+}
+</style>
--- version2/layouts/default.vue
+++ version2/layouts/default.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
+  <div class="wrapper">
+    <mobile-header></mobile-header>
     <nuxt />
   </div>
 </template>
 
+<script>
+import mobileHeader from "../components/header/mobile-header";
+
+export default {
+  name: "default-layout",
+  components: {
+    mobileHeader
+  }
+};
+</script>
+
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,400i,700&display=swap");
+
+:root {
+  --primary-font-face: "Open Sans", sans-serif;
+  --secondary-font-face: "Montserrat", sans-serif;
+  --primary-font-color: rgb(255, 251, 188);
+  --secondary-font-color: rgb(243, 236, 126);
+  --primary-background: rgb(26, 39, 52);
+  --off-white: rgb(230, 230, 190);
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -17,10 +45,28 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+body {
+  background: var(--primary-background);
+  font-family: var(--primary-font-face);
+  color: var(--primary-font-color);
+  min-height: 100vh;
+}
+
+a {
+  color: vary(--secondary-font-color);
+}
+
+a:link,
+a:visited {
+  color: var(--secondary-font-color);
+}
+
+a:hover {
+  color: var(--off-white);
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
 }
 </style>
