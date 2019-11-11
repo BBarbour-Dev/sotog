538d19c0c1506a09d44813b841dc38cb21510238
Brian Barbour <steelvoltage@gmail.com>
2019-11-11 11:29:40 -0500

    added contact form

--- sanity-sotog/schemas/author.js
+++ sanity-sotog/schemas/author.js
@@ -9,15 +9,6 @@ export default {
       type: 'string'
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 96
-      }
-    },
-    {
       name: 'image',
       title: 'Image',
       type: 'image',
